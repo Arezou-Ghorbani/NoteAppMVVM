6437424d896af51946abac38d2618a647f29a542
arz.ghorbani <Arezoughorbaniii@gmail.com>
2023-07-18 13:26:47 +0330

    note Fragment and main Activity added

--- app/src/main/java/com/example/noteappmvvm/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/noteappmvvm/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.noteappmvvm
+package com.example.noteappmvvm.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.noteappmvvm.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
