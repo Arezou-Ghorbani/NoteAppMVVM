fc573a27590773d1145d37ee5a452792a30f1ae7
arz.ghorbani <Arezoughorbaniii@gmail.com>
2023-07-19 09:03:09 +0330

    lambda function in Extentions.kt added

--- app/src/main/java/com/example/noteappmvvm/utils/di/Extentions.kt
+++ app/src/main/java/com/example/noteappmvvm/utils/di/Extentions.kt
@@ -4,14 +4,16 @@ import android.widget.ArrayAdapter
 import android.widget.Spinner
 import java.util.*
 
-fun Spinner.setUpListWithAdapter(list: MutableList<String>) {
+fun Spinner.setUpListWithAdapter(list: MutableList<String>, callBack: (String) -> Unit) {
     val adapter = ArrayAdapter(context, android.R.layout.simple_spinner_item, list)
     adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
     this.adapter = adapter
     this.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
         override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
 //      p2=position
-            list[p2]
+            callBack(
+                list[p2]
+            )
         }
 
         override fun onNothingSelected(p0: AdapterView<*>?) {
