59874288b9c69db777d3733ed34f4f34ff9fbbbb
arz.ghorbani <Arezoughorbaniii@gmail.com>
2023-07-19 09:29:20 +0330

    Spinner values filled

--- app/src/main/java/com/example/noteappmvvm/ui/main/note/NoteFragment.kt
+++ app/src/main/java/com/example/noteappmvvm/ui/main/note/NoteFragment.kt
@@ -5,16 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
 import com.example.noteappmvvm.R
 import com.example.noteappmvvm.databinding.FragmentNoteBinding
+import com.example.noteappmvvm.viewModel.NoteViewModel
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
+import setUpListWithAdapter
 
 @AndroidEntryPoint
 class NoteFragment : BottomSheetDialogFragment() {
     //    Binding
     private var _binding: FragmentNoteBinding? = null
     private val binding get() = _binding
+
+    //    ViewModel
+    private val viewModel: NoteViewModel by viewModels()
+
+    //    Other
+    private var category = ""
+    private var priority = ""
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
@@ -27,7 +38,21 @@ class NoteFragment : BottomSheetDialogFragment() {
         //        initViews
 
         binding?.apply {
-
+//Spinners
+//            categorySpinner
+            viewModel.loadCategoriesData()
+            viewModel.categoriesList.observe(viewLifecycleOwner) {
+                categoriesSpinner.setUpListWithAdapter(it) { itItem ->
+                    category = itItem
+                }
+            }
+//            prioritySpinner
+            viewModel.loadPriorityData()
+            viewModel.priorityList.observe(viewLifecycleOwner) {
+                prioritySpinner.setUpListWithAdapter(it) { itItem ->
+                    priority = itItem
+                }
+            }
         }
 
     }
--- app/src/main/java/com/example/noteappmvvm/viewModel/NoteViewModel.kt
+++ app/src/main/java/com/example/noteappmvvm/viewModel/NoteViewModel.kt
@@ -12,8 +12,8 @@ import javax.inject.Inject
 @HiltViewModel
 class NoteViewModel @Inject constructor() : ViewModel() {
     //    Spinners
-    private val categoriesList = MutableLiveData<MutableList<String>>()
-    private val priorityList = MutableLiveData<MutableList<String>>()
+     val categoriesList = MutableLiveData<MutableList<String>>()
+     val priorityList = MutableLiveData<MutableList<String>>()
     fun loadCategoriesData() = viewModelScope.launch(Dispatchers.IO) {
         val data = mutableListOf(WORK, EDUCATION, HOME, HEALTH)
         categoriesList.postValue(data)
